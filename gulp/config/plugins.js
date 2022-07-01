6531ebbe1b5aee3bfd7cfc1410056208f8c1cd9b
Lira Artem <artemlira84@gmail.com>
2022-07-01 23:51:32 +0300

    сделал до сборщика svg файлов, дальше не работает, выдает ошибку

---
+++ gulp/config/plugins.js
@@ -0,0 +1,17 @@
+import replace from "gulp-replace"; //Поиск и замена
+import plumber from "gulp-plumber"// Обработка шибок
+import notify from "gulp-notify" // Сщщбщение (подсказки)
+import browsersync from "browser-sync";// Локальный сервер
+import newer from "gulp-newer"; // Проверка обновлений
+import ifPlugin from "gulp-if" // условное ветвление
+
+
+//Экспортируем объект
+export const plugins = {
+   replace: replace,
+   plumber: plumber,
+   notify: notify,
+   browsersync: browsersync,
+   newer: newer,
+   if: ifPlugin
+}
