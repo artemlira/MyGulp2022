e8e7b20a1b677dee336cd35fd8cd6ad04effd3e2
Artem <artemlira84@gmail.com>
2023-06-28 15:20:21 +0300

    add Swiper implementation

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,41 @@
 import * as flsFunctions from "./modules/functions.js";
 import burgerMenu from "./modules/burger-menu.js"; //для бургер меню
 // import accordion from "./modules/accordion.js"; // для аккордиона
-import Swiper, { Navigation, Pagination } from "swiper";
 
 flsFunctions.isWebp();
 burgerMenu(); //для бургер меню
 // accordion(); // для аккордиона
 
-const swiper = new Swiper();
+// ==================== Swiper slider ====================
+//Подключение по CDN
+//Скопировать файл cdn.swiper.min.js в папку dist > js
+//В файл index.html перед подключением файла js добавить эту запись <script src="./js/cdn.swiper.min.js"></script>
+
+// const swiper = new Swiper(".swiper", {
+//   // Optional parameters
+//   direction: "horizontal", //ориентация прокрутки
+//   loop: true, //бесконечность
+//   spaceBetween: 80,  //расстояние между слайдами
+//   speed: 1000, //скорость прокрутки
+
+//   // Если нужна пагинация
+//   pagination: {
+//     el: ".swiper-pagination",
+//   },
+
+//   // Если нужны стрелки управления
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+
+//   // Если нужен scrollbar
+//   scrollbar: {
+//     el: ".swiper-scrollbar",
+//   },
+
+//   //Автозапуск
+//   // autoplay: {
+//   //   delay: 1800,
+//   // }
+// });
