d79e1e34ace77f60432dc00d5d6776cc54572b09
Artem <artemlira84@gmail.com>
2023-08-12 00:27:01 +0300

    made some minor changes, added a file with instructions

--- gulp/tasks/scss.js
+++ gulp/tasks/scss.js
@@ -1,58 +1,59 @@
-import dartSass from 'sass';
-import gulpSass from 'gulp-sass';
-import rename from 'gulp-rename';
+import dartSass from "sass";
+import gulpSass from "gulp-sass";
+import rename from "gulp-rename";
 
-import cleanCss from 'gulp-clean-css';
-import webpcss from 'gulp-webpcss';
-import autoprefixer from 'gulp-autoprefixer';
-import groupCssMediaQueries from 'gulp-group-css-media-queries';
+import cleanCss from "gulp-clean-css";
+import webpcss from "gulp-webpcss";
+import autoprefixer from "gulp-autoprefixer";
+import groupCssMediaQueries from "gulp-group-css-media-queries";
 
 const sass = gulpSass(dartSass);
 
 export const scss = () => {
-   return app.gulp.src(app.path.src.scss, { sourcemaps: app.isDev })
-      .pipe(app.plugins.plumber(
-         app.plugins.notify.onError({
+  return (
+    app.gulp
+      .src(app.path.src.scss, { sourcemaps: true })
+      .pipe(
+        app.plugins.plumber(
+          app.plugins.notify.onError({
             title: "SCSS",
-            message: "Error: <%= error.message %>"
-         }))
+            message: "Error: <%= error.message %>",
+          })
+        )
       )
-      .pipe(app.plugins.replace(/@img\//g, '../img/'))
-      .pipe(sass({
-         outputStyle: 'expanded'
-      }))
+      .pipe(app.plugins.replace(/@img\//g, "../img/"))
+      .pipe(
+        sass({
+          outputStyle: "expanded",
+        })
+      )
       .pipe(groupCssMediaQueries())
       .pipe(
-         app.plugins.if(
-            app.isBuild,
-            autoprefixer({
-               grid: true,
-               overrideBrowserslist: ["last 3 versions"],
-               cascade: true
-            })
-         )
+        app.plugins.if(
+          app.isBuild,
+          autoprefixer({
+            grid: true,
+            overrideBrowserslist: ["last 3 versions"],
+            cascade: true,
+          })
+        )
       )
-      .pipe(webpcss({
-         webpClass: ".webp",
-         noWebpClass: ".no-webp"
-      }))
-      // Закоментировать если НЕ НУЖЕН не сжатый дубль стилей. По умолчанию он создается только в режиме dev
       .pipe(
-         app.plugins.if(
-            app.isDev,
-            app.gulp.dest(app.path.build.css)
-         )
+        webpcss({
+          webpClass: ".webp",
+          noWebpClass: ".no-webp",
+        })
       )
-      // Раскоментровать если НУЖЕН не сжатый дубль стилей
-      .pipe(
-         app.plugins.if(
-            app.isBuild,
-            cleanCss()
-         )
-      )
-      .pipe(rename({
-         extname: ".min.css"
-      }))
+      // Закоментировать если НЕ НУЖЕН обычеый (не сжатый) дубль стилей.
       .pipe(app.gulp.dest(app.path.build.css))
-      .pipe(app.plugins.browsersync.stream());
-}
+      // сжатый дубль стилей
+      .pipe(cleanCss())
+      .pipe(
+        rename({
+          extname: ".min.css",
+        })
+      )
+      .pipe(app.gulp.dest(app.path.build.css))
+      .pipe(app.plugins.browsersync.stream())
+  );
+};
--- gulpfile.js
+++ gulpfile.js
@@ -4,17 +4,16 @@ import gulp from "gulp";
 import { path } from "./gulp/config/path.js";
 
 // Импорт общих плагинов
-import { plugins } from "./gulp/config/plugins.js"
-
+import { plugins } from "./gulp/config/plugins.js";
 
 //Передаем значения в глобальную переменную
 global.app = {
-   isBuild: process.argv.includes('--build'),
-   isDev: !process.argv.includes('--build'),
-   path: path,
-   gulp: gulp,
-   plugins: plugins
-}
+  isBuild: process.argv.includes("--build"),
+  isDev: !process.argv.includes("--build"),
+  path: path,
+  gulp: gulp,
+  plugins: plugins,
+};
 
 //Импорт задач
 import { copy } from "./gulp/tasks/copy.js";
@@ -25,34 +24,40 @@ import { scss } from "./gulp/tasks/scss.js";
 import { js } from "./gulp/tasks/js.js";
 import { images } from "./gulp/tasks/images.js";
 import { otfToTtf, ttfToWoff, fontsStyle } from "./gulp/tasks/fonts.js";
+import { svgSprive } from "./gulp/tasks/svgSprive.js";
 import { zip } from "./gulp/tasks/zip.js";
 
 //Наблюдатель за изменениями в файлах
 function watcher() {
-   gulp.watch(path.watch.files, copy);
-   gulp.watch(path.watch.html, html);
-   gulp.watch(path.watch.scss, scss);
-   gulp.watch(path.watch.js, js);
-   gulp.watch(path.watch.images, images);
+  gulp.watch(path.watch.files, copy);
+  gulp.watch(path.watch.html, html);
+  gulp.watch(path.watch.scss, scss);
+  gulp.watch(path.watch.js, js);
+  gulp.watch(path.watch.images, images);
 }
 
-
+export { svgSprive };
 
 //Последовательная обработка шрифтов
 const fonts = gulp.series(otfToTtf, ttfToWoff, fontsStyle);
 
 //Основные задачи
-const mainTasks = gulp.series(fonts, gulp.parallel(copy, html, scss, js, images));
+const mainTasks = gulp.series(
+  fonts,
+  gulp.parallel(copy, html, scss, js, images)
+);
 
 //Построение сценариев выполнения задач
 const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
 const build = gulp.series(reset, mainTasks, zip);
+// если нужно создавать архив отдельно, то нужно раскоментировать следующую запись
 // const deployZIP = gulp.series(reset, mainTasks, zip);
 
 //Экспорт сценариев
-export { dev }
-export { build }
+export { dev };
+export { build };
+// если нужно создавать архив отдельно, то нужно раскоментировать следующую запись
 // export { deployZIP }
 
 //Выполнение сценария по умолчанию
-gulp.task('default', dev);
+gulp.task("default", dev);
